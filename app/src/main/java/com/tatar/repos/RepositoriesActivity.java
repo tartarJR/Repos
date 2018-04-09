62588445806ce42940bc80263f3d48c131234c99
tartarJR <mustafaogun.ozturk@gmail.com>
2018-04-09 16:42:35 +0300

    initial commit

---
+++ app/src/main/java/com/tatar/repos/RepositoriesActivity.java
@@ -0,0 +1,13 @@
+package com.tatar.repos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepositoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repositories);
+    }
+}
