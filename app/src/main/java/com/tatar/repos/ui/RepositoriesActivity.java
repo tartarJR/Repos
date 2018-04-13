fce7c86b1fe8b1967ccbc1881af576157520b326
tartarJR <mustafaogun.ozturk@gmail.com>
2018-04-13 18:01:33 +0300

    first call to Github Api

--- app/src/main/java/com/tatar/repos/ui/RepositoriesActivity.java
+++ app/src/main/java/com/tatar/repos/ui/RepositoriesActivity.java
@@ -1,15 +1,43 @@
 package com.tatar.repos.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.tatar.repos.App;
 import com.tatar.repos.R;
+import com.tatar.repos.model.Repository;
+import com.tatar.repos.network.Service;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
 
 public class RepositoriesActivity extends AppCompatActivity {
+
+    Service service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repositories);
+
+        service = App.get(this).component().service();
+        Call<List<Repository>> reposCall = service.getAllRepos();
+        reposCall.enqueue(new Callback<List<Repository>>() {
+            @Override
+            public void onResponse(Call<List<Repository>> call, Response<List<Repository>> response) {
+                Timber.i("%s", response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Repository>> call, Throwable t) {
+                Toast.makeText(RepositoriesActivity.this, "Error getting repos " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
